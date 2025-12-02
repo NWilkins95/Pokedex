a0f2fa32ec32c580fd7d9924cd2f48486ca4499e
Nicholas Wilkins <150397998+NWilkins95@users.noreply.github.com>
2025-12-02 14:57:58 -0700

    Add validation middleware for routes and remove user route

--- src/routes/customPokemon.js
+++ src/routes/customPokemon.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const customPokemonController = require('../controllers/customPokemonController');
+const validate = require('../utils/validate');
 const { handleErrors } = require('../utils/error');
 const { checkLogin } = require('../utils/index');
 
 // Get all custom pokemons
-router.get('/', handleErrors(customPokemonController.getAllCustomPokemons));
+router.get('/', validate.validateGetAll, handleErrors(customPokemonController.getAllCustomPokemons));
 
 // Get a custom pokemon by ID
-router.get('/:id', handleErrors(customPokemonController.getCustomPokemonById));
+router.get('/:id', validate.validateCustomPokemonId, handleErrors(customPokemonController.getCustomPokemonById));
 
 // Get custom pokemons by name
-router.get('/name/:name', handleErrors(customPokemonController.getCustomPokemonByName));
+router.get('/name/:name', validate.validateCustomPokemonByName, handleErrors(customPokemonController.getCustomPokemonByName));
 
 // Create a new custom pokemon
-router.post('/', checkLogin, handleErrors(customPokemonController.createCustomPokemon));
+router.post('/', checkLogin, validate.validateCustomPokemonCreate, handleErrors(customPokemonController.createCustomPokemon));
 
 // Update a custom pokemon by ID
-router.put('/:id', checkLogin, handleErrors(customPokemonController.updateCustomPokemonById));
+router.put('/:id', checkLogin, validate.validateCustomPokemonUpdate, handleErrors(customPokemonController.updateCustomPokemonById));
 
 // Delete a custom pokemon by ID
-router.delete('/:id', checkLogin, handleErrors(customPokemonController.deleteCustomPokemonById));
+router.delete('/:id', checkLogin, validate.validateCustomPokemonId, handleErrors(customPokemonController.deleteCustomPokemonById));
 
 module.exports = router;
--- src/routes/pokemons.js
+++ src/routes/pokemons.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const pokemonController = require('../controllers/pokemonsController');
+const validate = require('../utils/validate');
 const { handleErrors } = require('../utils/error')
 
 // Get all pokemons
-router.get('/', handleErrors(pokemonController.getAllPokemons));
+router.get('/', validate.validateGetAll, handleErrors(pokemonController.getAllPokemons));
 
 // Get a pokemon by ID
-router.get('/:id', handleErrors(pokemonController.getPokemonById));
+router.get('/:id', validate.validatePokemonId, handleErrors(pokemonController.getPokemonById));
 
 module.exports = router;
